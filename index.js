3d1e13e7df1f6f6188211806a2600db4197bbb9a
Mikhail <mikhail.davlichin@kaspersky.com>
2023-01-28 20:37:41 +0200

    add stops

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ import { getCalendars } from "./src/getCalendars.js";
 import { getRoutes } from "./src/getRoutes.js";
 import { saveData } from "./src/saveData.js";
 import { getTrips } from "./src/getTrips.js";
+import { getStops } from "./src/getStops.js";
 
 (async () => {
   const agency = getAgency();
   const calendars = getCalendars();
   const routes = await getRoutes();
   const trips = await getTrips(routes, calendars);
+  const stops = await getStops(routes);
 
   saveData("agency", [agency]);
   saveData("calendar", calendars);
   saveData("routes", routes);
   saveData("trips", trips);
+  saveData("stops", stops);
 })();
---
+++ src/models/Stop.js
@@ -0,0 +1,7 @@
+export class Stop {
+  stop_id;
+  stop_name;
+  stop_lat;
+  stop_lon;
+  parent_station;
+}
