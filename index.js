8d912dad71a96348ad7941fb7c4590f0ae0d113f
Mikhail <mikhail.davlichin@kaspersky.com>
2023-01-28 17:40:32 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { saveAgency } from "./src/saveAgency.js";
-import fetch from "node-fetch";
 import { saveRoutes } from "./src/saveRoutes.js";
 
 //"https://api.cyprusbybus.com/dataservice/api/v1/routes/routedetail?routeid=40&languageId=1"
--- src/saveRoutes.js
+++ src/saveRoutes.js
@@ -1,6 +1,7 @@
 import { saveData } from "./saveData.js";
 import { Route } from "./models/Route.js";
 import { load } from "cheerio";
+import fetch from "node-fetch";
 
 export async function saveRoutes() {
   const routes = [];
@@ -10,12 +11,13 @@ export async function saveRoutes() {
   const $ = load(text);
   const links = $("#ctl00_ContentPlaceHolder1_gvRoutes tr td:first-child a");
   links.each((index, link) => {
-    const url = link.attribs["href"];
-    const id = url.substring(url.indexOf("?id=") + 4);
-    const name = $(link).text().trim();
-
     const route = new Route();
+    const url = link.attribs["href"];
+    route.route_id = url.substring(url.indexOf("?id=") + 4);
+    const [shortName, longName] = $(link).text().trim().split(",");
+    route.route_short_name = shortName.trim();
+    route.route_long_name = longName.trim();
+    routes.push(route);
   });
-
-  return saveData("routes", [routes]);
+  return saveData("routes", routes);
 }
